770f51b0ba563fe215eaf0e58adec0520562703e
huyang <972603060@qq.com>
2021-06-21 17:30:53 +0800

    spring mvc -> 接收客户端请求参数（普通类型、数组、对象、集合@RequestBody+json形式）

---
+++ spring_mvc/src/main/java/com/yang/controller/ParamController.java
@@ -0,0 +1,36 @@
+package com.yang.controller;
+
+import com.yang.bean.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: huyang
+ * @Date: 2021/6/21 下午4:59
+ */
+@RestController
+public class ParamController {
+
+    @GetMapping("/param")
+    String param(@RequestParam("id") String name, int age){
+        return name + age;
+    }
+
+    @GetMapping("/paramObj")
+    String paramObj(User user){
+        return user.toString();
+    }
+
+    @GetMapping("/params")
+    String params(String[] names){
+        return Arrays.toString(names);
+    }
+
+    @PostMapping("/paramList")
+    User paramList(@RequestBody List<User> userList){
+        return userList.get(0);
+    }
+}
